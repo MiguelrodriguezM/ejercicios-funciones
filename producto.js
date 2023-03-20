9a67894eca8d8ac8f97dca5779e601050b2e9c1f
MiguelrodriguezM <114486153+MiguelrodriguezM@users.noreply.github.com>
2023-03-20 11:12:45 +0100

    Add files via upload

---
+++ producto.js
@@ -0,0 +1,7 @@
+function producto(x1,x2){
+return x1 * x2;
+}
+
+let res_product = producto(3,7);
+
+console.log("El resultado de multiplicar 3 y 7 es " + res_product);
---
+++ saludo.js
@@ -0,0 +1,7 @@
+let nombre="Gabriel";
+function saluda(nombre){
+return "Hola " + nombre + ", ¿Que tal te va ?" ;
+}
+
+let mensaje = saluda("Gabriel");
+console.log(mensaje);
